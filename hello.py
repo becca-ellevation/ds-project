b18eb9034924b6cf51e9d2d461d490920d02f16a
Rebecca Cohn <rebecca.cohn@ellevationeducation.com>
2020-11-03 21:09:24 -0500

    Sketch out basic form template

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, json, jsonify, Response
+from flask import Flask, request, json, jsonify, Response, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World"
+    return render_template('hello.html')
 
 @app.route('/post', methods=['POST'])
 def post_route():
